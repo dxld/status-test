1f2f2f5510b2a49aa6cbb4ae77df1b898ccc9c7d
Daniel Gröber <dxld@darkboxed.org>
2017-01-16 04:27:23 +0100

    Make github-status shut up

--- github-status.sh
+++ github-status.sh
@@ -2,4 +2,4 @@
 status=$1
 # pending,
 
-curl --request POST --data '{"state": "'$status'", "target_url": "https://gitlab.com/'$CI_PROJECT_PATH'", "description": "Build '$status'", "context": "continuous-integration/gitlab-ci"}' -H "Authorization: token $GITHUB_STATUS_TOKEN" https://api.github.com/repos/DanielG/status-test/statuses/$CI_BUILD_REF
+curl --request POST --data '{"state": "'$status'", "target_url": "https://gitlab.com/'$CI_PROJECT_PATH'", "description": "Build '$status'", "context": "continuous-integration/gitlab-ci"}' -H "Authorization: token $GITHUB_STATUS_TOKEN" https://api.github.com/repos/DanielG/status-test/statuses/$CI_BUILD_REF >/dev/null
