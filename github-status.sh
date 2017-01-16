9b744c9b651da7122392030cda17c2d4e0ae7da9
Daniel Gröber <dxld@darkboxed.org>
2017-01-16 04:32:15 +0100

    Better URL

--- github-status.sh
+++ github-status.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 status=$1
+pipeline=$2
 # pending,
 
-curl --request POST --data '{"state": "'$status'", "target_url": "https://gitlab.com/'$CI_PROJECT_PATH'", "description": "Build '$status'", "context": "continuous-integration/gitlab-ci"}' -H "Authorization: token $GITHUB_STATUS_TOKEN" https://api.github.com/repos/DanielG/status-test/statuses/$CI_BUILD_REF >/dev/null
+curl --request POST --data '{"state": "'$status'", "target_url": "https://gitlab.com/'$CI_PROJECT_PATH/pipelines/$CI_PIPELINE_ID/'", "description": "GitlabCI build '$status'", "context": "continuous-integration/gitlab-ci"}' -H "Authorization: token $GITHUB_STATUS_TOKEN" https://api.github.com/repos/DanielG/status-test/statuses/$CI_BUILD_REF >/dev/null
